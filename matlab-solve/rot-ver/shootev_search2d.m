f1d58a47c80cbb080758b9e577b3acf21e3cd288
ld066077 <ld066077@gmail.com>
2025-03-08 20:01:52 +0800

    M=0.5结果

--- matlab-solve/rot-ver/shootev_search2d.m
+++ matlab-solve/rot-ver/shootev_search2d.m
@@ -7,8 +7,8 @@ function shootev_search2d()
     end
 
         % 定义参数扫描范围
-        realomg = linspace(2.2, 3.2, 100);    % 实部扫描范围
-        imagomg = linspace(-12.0e-2, -5.0e-10,100); % 虚部扫描范围
+        realomg = linspace(1.8, 3.1, 100);    % 实部扫描范围
+        imagomg = linspace(-15.0e-2, -5.0e-10,100); % 虚部扫描范围
         [Re, Im] = meshgrid(realomg, imagomg);    % 生成网格
         
     % 将二维网格展平为一维数组以便并行处理
